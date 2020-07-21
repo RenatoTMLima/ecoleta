b1c90bba1d6b1209b6db7dc84d53036fa56685e3
VORTX\rm <rm@vortx.com.br>
2020-07-21 13:20:34 -0300

    Finished frontend web

--- frontend/src/pages/CreatePoint/index.tsx
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState, ChangeEvent } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import api from '../../services/api';
@@ -34,6 +34,15 @@ const CreatePoint: React.FC = () => {
 
    const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0]);
    const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+
+   const [selectedItems, setSelectedItems] = useState<number[]>([]);
+   const [formData, setFormData] = useState({
+      name: '',
+      email: '',
+      whatsapp: ''
+   });
+
+   const history = useHistory();
 
    useEffect(() => {
       navigator.geolocation.getCurrentPosition(position => {
@@ -90,6 +99,51 @@ const CreatePoint: React.FC = () => {
       ])
    }
 
+   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+      const { name, value } = event.target;
+
+      setFormData({ ...formData, [name]: value });
+   }
+
+   const handleSelectItem = (id: number) => {
+      const alreadySelected = selectedItems.findIndex(item => item === id);
+
+      if(alreadySelected >= 0) {
+         const filteredItems = selectedItems.filter(item => item !== id);
+
+         setSelectedItems(filteredItems);
+      } else {
+         setSelectedItems([ ...selectedItems, id ]);
+      }
+   }
+
+   const handleSubmit = async (event: FormEvent) => {
+      event.preventDefault();
+
+      const {name, email, whatsapp} = formData;
+      const uf = selectedUf;
+      const city = selectedCity;
+      const [latitude, longitude] = selectedPosition;
+      const items = selectedItems;
+
+      const data = {
+         name, 
+         email, 
+         whatsapp, 
+         uf, 
+         city, 
+         latitude, 
+         longitude, 
+         items
+      }
+
+      await api.post('points', data);
+
+      alert('Ponto de coleta criado com sucesso!');
+
+      history.push('/');
+   }
+
    return (
       <div id="page-create-point">
          <header>
@@ -101,7 +155,7 @@ const CreatePoint: React.FC = () => {
             </Link>
          </header>
 
-         <form>
+         <form onSubmit={handleSubmit}>
             <h1>Cadastro do <br/> ponto de coleta</h1>
 
             <fieldset>
@@ -115,6 +169,7 @@ const CreatePoint: React.FC = () => {
                      type="text"
                      name="name"
                      id="name"
+                     onChange={handleInputChange}
                   />
                </div>
 
@@ -125,6 +180,7 @@ const CreatePoint: React.FC = () => {
                         type="email"
                         name="email"
                         id="email"
+                        onChange={handleInputChange}
                      />
                   </div>
                   <div className="field">
@@ -133,6 +189,7 @@ const CreatePoint: React.FC = () => {
                         type="text"
                         name="whatsapp"
                         id="whatsapp"
+                        onChange={handleInputChange}
                      />
                   </div>
                </div>
@@ -183,7 +240,11 @@ const CreatePoint: React.FC = () => {
 
                <ul className="items-grid">
                   {items.map(item => (
-                     <li key={item.id}>
+                     <li 
+                        key={item.id} 
+                        onClick={() => handleSelectItem(item.id)}
+                        className={selectedItems.includes(item.id) ? 'selected' : ''}
+                     >
                         <img src={item.image_url} alt={item.title}/>
                         <span>{item.title}</span>
                      </li>
@@ -200,7 +261,3 @@ const CreatePoint: React.FC = () => {
 }
 
 export default CreatePoint;
-
-/**
- *  1:51:20
- */
